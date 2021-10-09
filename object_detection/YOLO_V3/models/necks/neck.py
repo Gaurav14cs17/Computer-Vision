169a6043f8126883f115a64b26fa82124493536b
Gaurav14cs17 <gouravkumar717@gmail.com>
2021-10-09 22:51:28 +0530

    update neck

--- object_detection/YOLO_V3/models/necks/neck.py
+++ object_detection/YOLO_V3/models/necks/neck.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import sys
 from pathlib import Path
-
 file = Path(__file__).resolve()
 print(file)
 parent, root = file.parent, file.parents[1]
@@ -34,7 +33,7 @@ class Route(nn.Module):
 class Get_block(nn.Module):
     def __init__(self, feature_input_channels, out_channels, feature_output_channels):
         super(Get_block, self).__init__()
-        self.cfg = [(1, 1, 0), (3, 1, 1), (1, 1, 0), (3, 1, 1), (1, 1, 0)]
+        self.cfg = [(1, 1, 0), (3, 1, 1), (1, 1, 0), (3, 1, 1), (1, 1, 0), (3, 1, 1)]
         self.__in_channels = feature_input_channels
         self.__out_channels = out_channels
 
@@ -65,14 +64,14 @@ class Get_block(nn.Module):
 
         self.__in_channels = self.__out_channels
         self.__out_channels = self.__out_channels * 2
-        self.conv_6 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[4][0],
-                                    stride=self.cfg[4][1], padding=self.cfg[4][2], batch_norm='bn', activate='leaky')
+        self.conv_6 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[5][0],
+                                    stride=self.cfg[5][1], padding=self.cfg[5][2], batch_norm='bn', activate='leaky')
 
         self.__in_channels = self.__out_channels
         self.__out_channels = feature_output_channels
 
-        self.last = Convolutional(self.__in_channels, feature_output_channels, kernel_size=self.cfg[4][0],
-                                  stride=self.cfg[4][1], padding=self.cfg[4][2], batch_norm='bn', activate='leaky')
+        self.last = Convolutional(self.__in_channels, feature_output_channels, kernel_size=1,
+                                  stride=1, padding=0, batch_norm='bn', activate='leaky')
 
     def forward(self, x):
         x = self.conv_1(x)
@@ -85,14 +84,47 @@ class Get_block(nn.Module):
         return x
 
 
+class Feature_block(nn.Module):
+    def __init__(self, feature_input_channels, out_channels, feature_output_channels):
+        super(Feature_block, self).__init__()
+        self.__in_channels = feature_input_channels
+        self.__out_channels = out_channels
+        self.cfg = [(1, 1, 0), (3, 1, 1), (1, 1, 0), (3, 1, 1), (1, 1, 0), (3, 1, 1)]
+        self.model_list = nn.ModuleList()
+        flag = 0
+        for i in range(len(self.cfg)):
+            x = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[i][0],
+                              stride=self.cfg[i][1], padding=self.cfg[i][2], batch_norm='bn',
+                              activate='leaky')
+            self.model_list.append(x)
+            self.__in_channels = self.__out_channels
+            if not flag:
+                self.__out_channels = self.__out_channels * 2
+            else:
+                self.__out_channels = self.__out_channels // 2
+            flag = flag ^ 1
+
+        self.last_layer = Convolutional(self.__in_channels, feature_output_channels, kernel_size=1,
+                                        stride=1, padding=0, batch_norm='bn', activate='leaky')
+
+    def forward(self, x):
+        for layer in self.model_list:
+            x = layer(x)
+        x = self.last_layer(x)
+        return x
+
+
 if __name__ == '__main__':
     from torchsummary import summary
+
     model = Get_block(54, 128, 53)
+    model_1 = Feature_block(54, 128, 53)
     image = torch.randn((1, 54, 50, 50))
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    image = image.to(device)
-    model.cuda()
-    summary(model, (54, 50, 50))
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # image = image.to(device)
+    # model.cuda()
+    # summary(model, (54, 50, 50))
     output = model(image)
+    output_1 = model_1(image)
     print("\n\n")
     print(output.shape)
