fca368c4f96670b1fbdb3086f94089cc2d520940
Gaurav14cs17 <gouravkumar717@gmail.com>
2021-10-09 22:30:26 +0530

    update neck

--- object_detection/YOLO_V3/models/necks/neck.py
+++ object_detection/YOLO_V3/models/necks/neck.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 import sys
-from pathlib import Path  # if you haven't already done so
+from pathlib import Path
 
 file = Path(__file__).resolve()
 print(file)
@@ -33,60 +32,67 @@ class Route(nn.Module):
 
 
 class Get_block(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, feature_input_channels, out_channels, feature_output_channels):
         super(Get_block, self).__init__()
         self.cfg = [(1, 1, 0), (3, 1, 1), (1, 1, 0), (3, 1, 1), (1, 1, 0)]
-        self.__in_channels = in_channels
-        self.__out_channels = 2 * in_channels
+        self.__in_channels = feature_input_channels
+        self.__out_channels = out_channels
 
         self.conv_1 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[0][0],
                                     stride=self.cfg[0][1], padding=self.cfg[0][2], batch_norm='bn', activate='leaky')
 
         self.__in_channels = self.__out_channels
-        self.__out_channels =  self.__out_channels//2
+        self.__out_channels = self.__out_channels * 2
 
         self.conv_2 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[1][0],
                                     stride=self.cfg[1][1], padding=self.cfg[1][2], batch_norm='bn', activate='leaky')
 
         self.__in_channels = self.__out_channels
-        self.__out_channels = self.__out_channels *2
+        self.__out_channels = self.__out_channels // 2
 
         self.conv_3 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[2][0],
                                     stride=self.cfg[2][1], padding=self.cfg[2][2], batch_norm='bn', activate='leaky')
         self.__in_channels = self.__out_channels
-        self.__out_channels = self.__out_channels//2
+        self.__out_channels = self.__out_channels * 2
 
         self.conv_4 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[3][0],
                                     stride=self.cfg[3][1], padding=self.cfg[3][2], batch_norm='bn', activate='leaky')
         self.__in_channels = self.__out_channels
-        self.__out_channels = self.__out_channels* 2
+        self.__out_channels = self.__out_channels // 2
 
         self.conv_5 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[4][0],
                                     stride=self.cfg[4][1], padding=self.cfg[4][2], batch_norm='bn', activate='leaky')
 
+        self.__in_channels = self.__out_channels
+        self.__out_channels = self.__out_channels * 2
+        self.conv_6 = Convolutional(self.__in_channels, self.__out_channels, kernel_size=self.cfg[4][0],
+                                    stride=self.cfg[4][1], padding=self.cfg[4][2], batch_norm='bn', activate='leaky')
+
+        self.__in_channels = self.__out_channels
+        self.__out_channels = feature_output_channels
+
+        self.last = Convolutional(self.__in_channels, feature_output_channels, kernel_size=self.cfg[4][0],
+                                  stride=self.cfg[4][1], padding=self.cfg[4][2], batch_norm='bn', activate='leaky')
+
     def forward(self, x):
         x = self.conv_1(x)
-        print(x.shape)
         x = self.conv_2(x)
-        print(x.shape)
         x = self.conv_3(x)
-        print(x.shape)
         x = self.conv_4(x)
-        print(x.shape)
         x = self.conv_5(x)
-        print(x.shape)
+        x = self.conv_6(x)
+        x = self.last(x)
         return x
 
 
 if __name__ == '__main__':
     from torchsummary import summary
-
-    model = Get_block(512)
-
-    image = torch.randn((1, 512, 50, 50))
+    model = Get_block(54, 128, 53)
+    image = torch.randn((1, 54, 50, 50))
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     image = image.to(device)
     model.cuda()
-    summary(model, (512, 50, 50) )
+    summary(model, (54, 50, 50))
     output = model(image)
+    print("\n\n")
     print(output.shape)
